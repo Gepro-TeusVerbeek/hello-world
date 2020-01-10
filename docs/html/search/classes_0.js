9561e10e44e9618cf69d9439eb1da338214bd0ce
Teus Verbeek <teus@gepro-electronics.nl>
2020-01-10 14:07:38 +0100

    Runned doxygen again

---
+++ docs/html/search/classes_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['mathresult_14',['MathResult',['../struct_math_utils_1_1_base_1_1_math_result.html',1,'MathUtils::Base']]]
+];
---
+++ docs/html/search/namespaces_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['mathutils_16',['MathUtils',['../namespace_math_utils.html',1,'']]],
+  ['solvers_17',['Solvers',['../namespace_math_utils_1_1_solvers.html',1,'MathUtils']]]
+];
